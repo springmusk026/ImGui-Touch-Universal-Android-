1b5b74004b7a5254b5bc7427ae9c49ff66a1ecea
Musk Modder <85071855+springmusk026@users.noreply.github.com>
2022-07-28 13:29:13 +0545

    Update touch.c

--- touch.c
+++ touch.c
@@ -1,1 +1,13 @@
 
+void orig_input(void *thiz, void *ex_ab, void *ex_ac);
+void h_input(void *thiz, void *ex_ab, void *ex_ac){
+    orig_input(thiz,ex_ac,ex_ac);
+    ImGui_ImplAndroid_HandleInputEvent((AInputEvent *)thiz); 
+    return;
+}
+
+void *sys_in = DobbySymbolResolver(OBFUSCATE("/system/lib/libinput.so"), OBFUSCATE("_ZN7android13InputConsumer21initializeMotionEventEPNS_11MotionEventEPKNS_12InputMessageE"));
+    
+if (sys_in != NULL){
+   DobbyHook((void *)sys_in, (void *) h_input, (void **)&orig_input);
+}
