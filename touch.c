29a1599ac5f9f5298f66aa3140d8cef544043882
Musk Modder <85071855+springmusk026@users.noreply.github.com>
2022-07-29 19:56:34 +0545

    Update touch.c

--- touch.c
+++ touch.c
@@ -3,9 +3,9 @@
     ret (*orig##func)(__VA_ARGS__); \
     ret my##func(__VA_ARGS__)
 
-HOOK(void, Input, void *thiz, void *ex_ab, void *ex_ac){ // 触摸设置
+HOOK(void, Input, void *thiz, void *ex_ab, void *ex_ac){ 
     origInput(thiz, ex_ab, ex_ac);
-    ImGui_ImplAndroid_HandleInputEvent((AInputEvent *)thiz); // 处理imgui输入
+    ImGui_ImplAndroid_HandleInputEvent((AInputEvent *)thiz); 
     return;
 }
 
@@ -14,7 +14,7 @@ JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *vm, void * reserved)
     JNIEnv *env;
     vm->GetEnv((void **) &env, JNI_VERSION_1_6);
 
-    void *sym_input = DobbySymbolResolver(("/system/lib/libinput.so"), ("_ZN7android13InputConsumer21initializeMotionEventEPNS_11MotionEventEPKNS_12InputMessageE")); // 获取input接收器
+    void *sym_input = DobbySymbolResolver(("/system/lib/libinput.so"), ("_ZN7android13InputConsumer21initializeMotionEventEPNS_11MotionEventEPKNS_12InputMessageE")); 
     if (NULL != sym_input){
         DobbyHook((void *)sym_input, (void *) myInput, (void **)&origInput);
     }
